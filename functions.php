a0252192ee287e3eed9a1994f64ee1abde48b233
LaylaR <layla.richard.2111@gmail.com>
2023-10-20 10:17:12 +0200

    functions exercices

--- functions.php
+++ functions.php
@@ -1,16 +1,54 @@
 <?php
+//Acronym
+
+//sum
+
+try{
+ echo "<p>" .add('a',5). "</p>";
+}
+
+catch (Exception $e){
+    echo $e->getMessage();
+}
+
+/**
+ * @throws Exception
+ */
+function add($a, $b) : float{
+
+    if(!is_numeric($a) || !is_numeric($b)){
+        throw new Exception("the arguments are not of the correct type");
+    }
+
+    return $a+$b;
+}
+//Current Date
+echo "<p>" . date('d/m/Y H:i:s'). "</p>";
+
+//Current year
+echo "<p>" .date('Y')."</p>";
+//Capitalize
+
+$res = capitalise("test");
+echo "<p>$res</p>";
+function capitalise($word) :string
+{
+    return ucfirst($word);
+
+}
+
+//Shuffle sentence
 
 function sentence_shuffle($sentence): string
 {
 
     $words = str_split($sentence);
-    $shuffleWords=[];
+    $shuffleWords = [];
 
-    foreach($words as $word){
+    foreach ($words as $word) {
 
         $shuffleWords[] = (str_shuffle($word));
     }
-
 
 
     return implode($shuffleWords);
